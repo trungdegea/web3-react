ddeab6faf680c12dd63795b24dc5e1b64c996b10
trungdegea <trungdegea30032000@gmail.com>
2024-03-08 00:50:50 +0700

    init src

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useWeb3React } from "@web3-react/core";
+import { ethers } from "ethers";
+import { useEffect, useState } from "react";
+import "./App.css";
+import { injected } from "./connector";
 
 function App() {
+  const [accounts, setAccounts] = useState("hi");
+  const { active, account, library, connector, chainId, activate, deactivate } =
+    useWeb3React();
+
+  useEffect(() => {
+    const load = async () => {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      console.log(provider);
+      const signer = provider.getSigner();
+      console.log(signer);
+      await provider.getBlockNumber();
+      let balance = await provider.getBalance(
+        "0xF53593e0009582B9ADac9F2854ce07ACDcde94EC"
+      );
+      console.log(ethers.utils.formatEther(balance));
+      console.log(provider._network);
+      // const tx = signer.sendTransaction({
+      //   to: "0xA2397895a57E90b8BA240415b29c05214E533B07",
+      //   value: ethers.utils.parseEther("0.1"),
+      // });
+    };
+    load();
+
+    setAccounts(account);
+  }, [account]);
+  const connect = async () => {
+    try {
+      await activate(injected);
+    } catch (ex) {
+      console.log(ex);
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>account : {accounts}</h1>
+      <button onClick={connect}>connect to metaMark</button>
+      {active ? (
+        <span>
+          Connected with <b>{account}</b>
+        </span>
+      ) : (
+        <span>Not connected</span>
+      )}
     </div>
   );
 }
